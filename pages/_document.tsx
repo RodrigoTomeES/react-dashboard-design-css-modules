3fc3956e5381da7cdae37f2a8ddfe9dd0ff4705d
RodrigoTomeES <rotome@unirioja.es>
2021-09-16 10:16:00 +0200

    Included CssBaseline in _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
-import { CssBaseline } from '@geist-ui/react';
+
+import CssBaseline from '@/components/css-baseline';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
-    const styles = CssBaseline.flush();
 
     return {
       ...initialProps,
       styles: (
         <>
           {initialProps.styles}
-          {styles}
+          <CssBaseline />
         </>
       )
     };
